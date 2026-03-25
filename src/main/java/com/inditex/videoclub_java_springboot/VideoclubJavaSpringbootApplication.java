3b747441287794340b38cb601717cf1df2f10fbc
Ana Morandeira <ana.agrodorolo@gmail.com>
2026-03-25 19:09:06 +0100

    initial commit

---
+++ src/main/java/com/inditex/videoclub_java_springboot/VideoclubJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.videoclub_java_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoclubJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoclubJavaSpringbootApplication.class, args);
+	}
+
+}
