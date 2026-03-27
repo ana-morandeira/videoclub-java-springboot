bfaeb06517fad569d0a4d141a0a8a9d22e385899
Ana Morandeira <ana.agrodorolo@gmail.com>
2026-03-27 10:44:53 +0100

    feat: update in MovieController

--- src/main/java/com/inditex/videoclub_java_springboot/controller/MovieController.java
+++ src/main/java/com/inditex/videoclub_java_springboot/controller/MovieController.java
@@ -2,9 +2,12 @@ package com.inditex.videoclub_java_springboot.controller;
 
 import com.inditex.videoclub_java_springboot.model.Movie;
 import com.inditex.videoclub_java_springboot.service.MovieService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class MovieController {
@@ -35,4 +38,24 @@ public class MovieController {
         return movieService.getAllByOrder();
     }
 
+    @GetMapping("/movies/{id}")
+    public ResponseEntity<Movie> findMovieById(@PathVariable int id){
+        Optional<Movie> foundMovie = movieService.findMovie(id);
+        if(foundMovie.isPresent()){
+            return new ResponseEntity<>(foundMovie.get(), HttpStatus.FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/movies/{id}")
+    public ResponseEntity<Movie> updateMovieById(@PathVariable int id, @RequestBody Movie updatedMovie){
+        try{
+            // Actualizo los campos si existe el id en la BBDD
+            Movie movie = movieService.updateMovie(id, updatedMovie);
+            return new ResponseEntity<>(movie, HttpStatus.OK);
+        }
+        catch(Exception exception){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
